f0c8ca877e5c019982cc2d600116b171fa5c93c7
Nate-K0 <nkenopic@myseneca.ca>
2023-04-26 22:08:19 -0400

    first commit

---
+++ bank-app.py
@@ -0,0 +1,15 @@
+print("=========================================================")
+print("============= Welcome To Nate's Banking App =============")
+print("=========================================================")
+
+print("0. Exit")
+print("1. See Account Balance")
+print("2. Make Deposit")
+print("3. Make Withdrawal")
+
+print("")
+
+response = input("Pick number: ")
+
+if response == 0:
+	exit()
